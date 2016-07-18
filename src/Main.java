9836e71a2e31b989c40b0804110307c2d02b44d6
argallo <argallo@wpi.edu>
2016-07-17 23:49:06 -0400

    updates

--- src/Main.java
+++ src/Main.java
@@ -47,51 +47,5 @@ public class Main {
         mirror.pack();
         mirror.setVisible(true);
     }
-
-/*    static public boolean fullScreen(final JFrame frame, boolean doPack) {
-
-        GraphicsDevice device = frame.getGraphicsConfiguration().getDevice();
-        boolean result = device.isFullScreenSupported();
-
-        if (result) {
-            frame.setUndecorated(true);
-            frame.setResizable(true);
-
-            frame.addFocusListener(new FocusListener() {
-
-                @Override
-                public void focusGained(FocusEvent arg0) {
-                    frame.setAlwaysOnTop(true);
-                }
-
-                @Override
-                public void focusLost(FocusEvent arg0) {
-                    frame.setAlwaysOnTop(false);
-                }
-            });
-
-            if (doPack)
-                frame.pack();
-
-            device.setFullScreenWindow(frame);
-        }
-        else {
-            frame.setPreferredSize(frame.getGraphicsConfiguration().getBounds().getSize());
-
-            if (doPack)
-                frame.pack();
-
-            frame.setResizable(true);
-
-            frame.setExtendedState(Frame.MAXIMIZED_BOTH);
-            boolean successful = frame.getExtendedState() == Frame.MAXIMIZED_BOTH;
-
-            frame.setVisible(true);
-
-            if (!successful)
-                frame.setExtendedState(Frame.MAXIMIZED_BOTH);
-        }
-        return result;
-    }*/
-
+    
 }
